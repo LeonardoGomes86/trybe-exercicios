2bbc89a083c97ca54edc1bbe04bcd703875ee8d2
LeonardoGomes86 <leonardogomes.adm@hotmail.com>
2022-09-21 19:35:49 -0300

    criado programa para verificar maior número entre 2 numeros

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
@@ -24,3 +24,13 @@ const a = 4;
 const b = 2;
 let mod = a % b;
 console.log(mod);
+// Programa 05/15 - Utilize if/else para fazer um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+const a = 10;
+const b = 15;
+if (a > b) {
+  console.log("Número " + a + " é maior que o número " + b);
+} else if {
+  console.log("Número " + b + " é maior que o número " + a);
+} else {
+  console.log("Números são iguais");
+}
