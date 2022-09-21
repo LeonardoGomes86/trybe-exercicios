97e12f7d50c126613d5d013bdfe16e9be8286260
LeonardoGomes86 <leonardogomes.adm@hotmail.com>
2022-09-21 18:27:23 -0300

    Se nossa variável  weekDay  for  segunda-feira ,  terça-feira ,  quarta-feira , quinta-feira ou sexta-feira, imprima Oba, mais um dia de aprendizado na Trybe >:D

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts5.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts5.js
@@ -1,1 +1,4 @@
-let weekDay = "quarta-feira";
+let weekDay = "domingo";
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
