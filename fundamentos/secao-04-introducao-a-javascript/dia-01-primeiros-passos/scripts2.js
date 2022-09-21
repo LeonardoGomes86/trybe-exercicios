2dd7c2266c324931ce53b1919ea129fb3db097d1
LeonardoGomes86 <leonardogomes.adm@hotmail.com>
2022-09-21 15:43:09 -0300

    Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts2.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts2.js
@@ -1,3 +1,7 @@
 // Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
 const base = 5;
+console.log(base);
 const heigth = 8;
+console.log(heigth);
+const area = base * heigth;
+console.log(area);
