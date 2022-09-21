a8dba89e7ed829886e0c8eb98064b94597702fd8
LeonardoGomes86 <leonardogomes.adm@hotmail.com>
2022-09-21 19:24:31 -0300

    criado programas para soma e subtração

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
@@ -1,1 +1,11 @@
 // Exercicios pós aula ao vivo
+// Programa 01/15 - programa para adição
+const a = 5;
+const b = 3;
+let sum = a + b;
+console.log(sum);
+// Programa 02/15 - programa para subtração
+const a = 10;
+const b = 3;
+let subtraction = a - b;
+console.log(subtraction);
