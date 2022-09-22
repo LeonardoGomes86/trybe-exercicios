d5b7a1eb6ac9134d1e2a43a3af9ff963d0ae14de
LeonardoGomes86 <leonardogomes.adm@hotmail.com>
2022-09-21 21:41:29 -0300

    criado programa que verifica 3 números e retorna true se pelo menos 1 for impar. Caso contrário, ele deve retornar false

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
@@ -150,3 +150,13 @@ if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
 } else {
   console.log(false);
 }
+// Programa 13/15 - tilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if.
+const a = 7;
+const b = 9;
+const c = 3;
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
