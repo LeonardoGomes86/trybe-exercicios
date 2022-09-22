a6d33db6c48873af2c932134c92f58dff5236f44
LeonardoGomes86 <leonardogomes.adm@hotmail.com>
2022-09-21 21:38:06 -0300

    criado programa que verifica 3 números e retorna true se pelo menos for par. Caso contrário, ele deve retornar false

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script7.js
@@ -140,3 +140,13 @@ if (notaPorcentagem >= 90) {
     console.log('Aluno(a) tirou nota ' + notaLetra);
   }
 }
+// Programa 12/15 - Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if. 
+const a = 3;
+const b = 5;
+const c = 7;
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
